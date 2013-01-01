925846901ea80109fdc58895dce7182a761cd712
Yaremenko Dmitry <deweisowl@gmail.com>
2013-01-01 12:39:34 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scenamix::Application.config.secret_token = '2b67eabe381e4e1f9c84c42b9033e36c419b63ef2d3d3d35ea7c51b47dc5450e62cd1d584383fbbfbd8398a5918427b9b675891baa503da93f33f8534d80b388'
