7100942af0328a1ce6dd189dea7de7227b7226c1
Yaremenko Dmitry <deweisowl@gmail.com>
2013-01-23 23:08:40 +0200

    added new specs for post_format - links representation and disallow to use html codes

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -7,5 +7,13 @@ describe ApplicationHelper do
       helper.post_format("first line\nsecond line").should eql("<p>first line\n<br />second line</p>")
       helper.post_format("first line\n\nsecond line").should eql("<p>first line</p>\n\n<p>second line</p>")
     end
+
+    it "transforms links" do
+
+    end
+
+    it "transforms html entities" do
+      helper.post_format("<b>bold</b>").should eql("<p>&lt;b&rt;bold&lt;/b&rt;</p>")
+    end
   end
 end
