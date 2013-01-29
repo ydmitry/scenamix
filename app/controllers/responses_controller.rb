8ae5bffd7816131225f7ef8cf220e2d344cd7069
Sergii Ponomarov <sergii.ponomarov@gmail.com>
2013-01-29 02:56:31 +0200

    Refactor controllers

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,31 +1,23 @@
 class ResponsesController < ApplicationController
   def create
-    @scene = Scene.find(params[:scene_id])
-    params_response = params[:response]
-    params_response[:scene_id] = params[:scene_id]
-    @response = Response.create!(params_response)
-      
-    flash[:notice] = "Response to #{@scene.title} was successfully posted."
-    redirect_to scene_path(@scene)
+    scene = Scene.find(params[:scene_id])
+    scene.responses.create!(params[:response])
+    redirect_to scene, notice: "Response to #{scene.title} was successfully posted."
   end
 
   def edit
-    @response = Response.find params[:id]
+    @response = Response.find(params[:id])
   end
 
   def update
-    @response = Response.find params[:id]
-    @scene = Scene.find(@response.scene_id)
-    @response.update_attributes!(params[:response])
-    flash[:notice] = "#Response was successfully updated."
-    redirect_to scene_path(@scene)
+    response = Response.find(params[:id])
+    response.update_attributes!(params[:response])
+    redirect_to response.scene, notice: "#Response was successfully updated."
   end
 
   def destroy
-    @response = Response.find(params[:id])
-    @scene = Scene.find(@response.scene_id)
-    @response.destroy
-    flash[:notice] = "Response deleted."
-    redirect_to scene_path(@scene)
+    response = Response.find(params[:id])
+    response.destroy
+    redirect_to response.scene, notice: "Response deleted."
   end
 end
--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -1,36 +1,32 @@
 class ScenesController < ApplicationController
-  def show
-    id = params[:id] # retrieve movie ID from URI route
-    @scene = Scene.find(id) # look up movie by unique ID
-    @responses = Response.find_all_by_scene_id(id)
-    # will render app/views/scenes/show.<extension> by default
-  end
-
   def index
     @scenes = Scene.all
   end
 
+  def new; end
+
   def create
     @scene = Scene.create!(params[:scene])
-    flash[:notice] = "#{@scene.title} was successfully created."
-    redirect_to scene_path(@scene)
+    redirect_to @scene, notice: "#{@scene.title} was successfully created."
+  end
+
+  def show
+    @scene = Scene.find(params[:id])
   end
 
   def edit
-    @scene = Scene.find params[:id]
+    @scene = Scene.find(params[:id])
   end
 
   def update
-    @scene = Scene.find params[:id]
+    @scene = Scene.find(params[:id])
     @scene.update_attributes!(params[:scene])
-    flash[:notice] = "#{@scene.title} was successfully updated."
-    redirect_to scene_path(@scene)
+    redirect_to @scene, notice: "#{@scene.title} was successfully updated."
   end
 
   def destroy
     @scene = Scene.find(params[:id])
     @scene.destroy
-    flash[:notice] = "Scene '#{@scene.title}' deleted."
-    redirect_to scenes_path
+    redirect_to scenes_path, notice: "Scene '#{@scene.title}' deleted."
   end
 end
