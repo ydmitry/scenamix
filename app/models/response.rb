d7296403d64df75416d5095654e199a21ba9b765
Yaremenko Dmitry <deweisowl@gmail.com>
2013-02-01 21:16:32 +0200

    Added tree structure to response

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,5 +1,7 @@
 class Response < ActiveRecord::Base
   attr_accessible :scene_id, :response
   belongs_to :scene
+  has_many :next, :foreign_key => "parent_id", :class_name => "Scenario"
+  belongs_to :previous, :class_name => "Scenario"
   validates :response, presence: true
 end
