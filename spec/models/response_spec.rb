3c98841e6f6c29b60183d8df30683048a5261fcc
Yaremenko Dmitry <deweisowl@gmail.com>
2013-02-02 21:31:44 +0200

    Alternative responses rspec

--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Response do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it 'should return alternative responses' do
+    fork = Response.create!(response: 'Fork', parent_id: 0)
+    turnright = Response.create!(response: 'Turn right', parent_id: fork.id)
+    turnleft = Response.create!(response: 'Turn left', parent_id: fork.id)
+
+    turnright.alternative.should include turnleft
+  end
 end
