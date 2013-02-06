68883fe1e011f74d8b49d0b58fc4739f7983df0f
Yaremenko Dmitry <deweisowl@gmail.com>
2013-02-06 20:47:58 +0200

    Best_scenario method

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -2,4 +2,8 @@ class Scene < ActiveRecord::Base
   attr_accessible :title, :description
   has_many :responses, :dependent => :destroy
   validates :title, :description, presence: true
+
+  def best_scenario
+   Response.where('scene_id = ?', self.id).select('id, scene_id, parent_id, response, upvotes, downvotes, MAX(upvotes - downvotes) as votes').group(:parent_id)
+  end
 end
