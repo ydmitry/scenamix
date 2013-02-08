0fe74c1f3dd309fb7e50552c520bb7206563ac7d
Yaremenko Dmitry <deweisowl@gmail.com>
2013-02-08 08:41:51 +0200

    Fixed parent and add ne conditions to scene model spec

--- spec/models/scene_spec.rb
+++ spec/models/scene_spec.rb
@@ -6,11 +6,13 @@ describe Scene do
     scene = FactoryGirl.create(:scene_physical_form)
     response_sport = FactoryGirl.create(:response_sport, scene: scene)
     response_alcohol = FactoryGirl.create(:response_alcohol, scene: scene)
-    response_sport_gym = FactoryGirl.create(:response_sport_gym, scene: scene)
-    response_sport_chess = FactoryGirl.create(:response_sport_chess, scene: scene)
+    response_sport_gym = FactoryGirl.create(:response_sport_gym, scene: scene, parent: response_sport)
+    response_sport_chess = FactoryGirl.create(:response_sport_chess, scene: scene, parent: response_sport)
     
     scene.best_scenario.should include response_sport_gym
     scene.best_scenario.should include response_sport
+    scene.best_scenario.should_not include response_alcohol
+    scene.best_scenario.should_not include response_sport_chess
   end
 
 end
