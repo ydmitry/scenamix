1e44763779b548a4cc7e1e7de1bac4e20f2e34d1
Yaremenko Dmitry <deweisowl@gmail.com>
2013-02-09 12:42:07 +0200

    More complicated test for best scenario

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -3,35 +3,44 @@ require 'factory_girl'
 FactoryGirl.define do
   factory :scene_physical_form, class: Scene do
     title 'Good physical form'
-    description 'How to get good physical form?'
+    description 'One man wants to improve his physical form.'
   end
 
   factory :response_sport, class: Response do
     association :scene, factory: :scene_physical_form    
-    response 'Engage in sports'
-    upvotes 1 
+    response 'He engages in sports'
+    upvotes 1
   end
 
   factory :response_alcohol, class: Response do
     association :scene, factory: :scene_physical_form    
-    response 'Drink alcohol'
+    response 'He begin to drink alcohol'
+    upvotes -1
   end
 
   factory :response_smoke, class: Response do
     association :scene, factory: :scene_physical_form    
     response 'And smoke cigarettes'
+    upvotes -1
+  end
+
+  factory :response_fastfood, class: Response do
+    association :scene, factory: :scene_physical_form    
+    response 'Or eat fastfood'
+    upvotes -1
   end
 
   factory :response_sport_gym, class: Response do
     association :scene, factory: :scene_physical_form
     association :parent, factory: :response_sport
-    response 'Go to gym'
-    upvotes 1
+    response 'He go to gym and improves'
+    upvotes 2
   end
 
   factory :response_sport_chess, class: Response do
     association :scene, factory: :scene_physical_form
     association :parent, factory: :response_sport
-    response 'Play chess'    
+    response 'And play chess. Not improves, but not worsen'
+    upvotes 1
   end
 end
--- spec/models/scene_spec.rb
+++ spec/models/scene_spec.rb
@@ -8,12 +8,16 @@ describe Scene do
     response_alcohol = FactoryGirl.create(:response_alcohol, scene: scene)
     response_sport_gym = FactoryGirl.create(:response_sport_gym, scene: scene, parent: response_sport)
     response_sport_chess = FactoryGirl.create(:response_sport_chess, scene: scene, parent: response_sport)
-    
+    response_smoke = FactoryGirl.create(:response_smoke, scene: scene, parent: response_alcohol)
+    response_fastfood = FactoryGirl.create(:response_fastfood, scene: scene, parent: response_alcohol)
+
     scenario = scene.best_scenario
     scenario.should include response_sport_gym
     scenario.should include response_sport
     scenario.should_not include response_alcohol
     scenario.should_not include response_sport_chess
+    scenario.should_not include response_smoke
+    scenario.should_not include response_fastfood
   end
 
   it 'should return best scenario if there is only one response in the scene' do
