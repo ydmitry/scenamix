5b26c954bdf9a54fd1171d7b2afcef113258c048
Andrii Ponomarov <andrii.ponomarov@gmail.com>
2013-02-10 03:03:14 +0200

    Refactor specs
    https://github.com/andreareginato/betterspecs/issues/15#issuecomment-9202057

--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -1,33 +1,35 @@
 require 'spec_helper'
 
 describe Response do
-  
-  it 'does return alternative responses' do
-    scene = create(:scene)
-    fork = create(:response, scene: scene)
-    turnright = create(:response, scene: scene, parent: fork)
-    turnleft = create(:response, scene: scene, parent: fork)
+  describe '#alternative' do
+    it 'returns alternative responses' do
+      scene = create(:scene)
+      fork = create(:response, scene: scene)
+      turnright = create(:response, scene: scene, parent: fork)
+      turnleft = create(:response, scene: scene, parent: fork)
 
-    turnright.alternative.should include turnleft
-    turnright.alternative.should_not include turnright
+      turnright.alternative.should include turnleft
+      turnright.alternative.should_not include turnright
+    end
   end
 
+  describe '#best_scenario' do
+    it 'returns best scenario for one response' do
+      scene = create(:scene)
+      response_sport = create(:response, scene: scene, upvotes: 1)
+      response_alcohol = create(:response, scene: scene)
+      response_sport_gym = create(:response, scene: scene, parent: response_sport, upvotes: 1)
+      response_sport_chess = create(:response, scene: scene, parent: response_sport)
+      response_alcohol_smoke = create(:response, scene: scene, parent: response_alcohol)
+      response_alcohol_smoke_fastfood = create(:response, scene: scene, parent: response_alcohol_smoke)
 
-  it 'does return best scenario for one response' do
-    scene = create(:scene)
-    response_sport = create(:response, scene: scene, upvotes: 1)
-    response_alcohol = create(:response, scene: scene)
-    response_sport_gym = create(:response, scene: scene, parent: response_sport, upvotes: 1)
-    response_sport_chess = create(:response, scene: scene, parent: response_sport)
-    response_alcohol_smoke = create(:response, scene: scene, parent: response_alcohol)
-    response_alcohol_smoke_fastfood = create(:response, scene: scene, parent: response_alcohol_smoke)
+      response_alcohol.best_scenario.should == [response_alcohol, response_alcohol_smoke, response_alcohol_smoke_fastfood]
 
-    response_alcohol.best_scenario.should == [response_alcohol, response_alcohol_smoke, response_alcohol_smoke_fastfood]
+      response_sport_chess.best_scenario.should == [response_sport, response_sport_chess]
 
-    response_sport_chess.best_scenario.should == [response_sport, response_sport_chess]
-
-    response_alcohol_smoke_fastfood.best_scenario.should == [
-      response_alcohol, response_alcohol_smoke, response_alcohol_smoke_fastfood
-    ]
+      response_alcohol_smoke_fastfood.best_scenario.should == [
+        response_alcohol, response_alcohol_smoke, response_alcohol_smoke_fastfood
+      ]
+    end
   end
 end
--- spec/models/scene_spec.rb
+++ spec/models/scene_spec.rb
@@ -1,30 +1,30 @@
 require 'spec_helper'
 
 describe Scene do
+  describe '#best_scenario' do
+    it 'returns best scenario' do
+      scene = create(:scene)
+      response_sport = create(:response, scene: scene, upvotes: 1)
+      response_alcohol = create(:response, scene: scene, upvotes: -1)
+      response_sport_gym = create(:response, scene: scene, parent: response_sport, upvotes: 1)
+      response_sport_chess = create(:response, scene: scene, parent: response_sport)
+      response_alcohol_smoke = create(:response, scene: scene, parent: response_alcohol)
+      response_alcohol_fastfood = create(:response, scene: scene, parent: response_alcohol)
 
-  it 'does return best scenario' do
-    scene = create(:scene)
-    response_sport = create(:response, scene: scene, upvotes: 1)
-    response_alcohol = create(:response, scene: scene, upvotes: -1)
-    response_sport_gym = create(:response, scene: scene, parent: response_sport, upvotes: 1)
-    response_sport_chess = create(:response, scene: scene, parent: response_sport)
-    response_alcohol_smoke = create(:response, scene: scene, parent: response_alcohol)
-    response_alcohol_fastfood = create(:response, scene: scene, parent: response_alcohol)
+      scene.best_scenario.should == [response_sport, response_sport_gym]
+    end
 
-    scene.best_scenario.should == [response_sport, response_sport_gym]
+    it 'returns best scenario if there is only one response in the scene' do
+      scene = create(:scene, title: 'Good physical form', description: 'One man wants to improve his physical form.')
+      response_sport = create(:response, scene: scene, response: 'He engages in sports')
+
+      scene.best_scenario.should include response_sport
+    end
+
+    it 'returns empty if there is no response in the scene' do
+      scene = create(:scene, title: 'Good physical form', description: 'One man wants to improve his physical form.')
+
+      scene.best_scenario.should be_empty
+    end
   end
-
-  it 'does return best scenario if there is only one response in the scene' do
-    scene = create(:scene, title: 'Good physical form', description: 'One man wants to improve his physical form.')
-    response_sport = create(:response, scene: scene, response: 'He engages in sports')
-
-    scene.best_scenario.should include response_sport
-  end
-
-  it 'does return empty if there is no response in the scene' do
-    scene = create(:scene, title: 'Good physical form', description: 'One man wants to improve his physical form.')
-
-    scene.best_scenario.should be_empty
-  end
-
 end
