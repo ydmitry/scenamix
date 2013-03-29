30d00fff5a4822e0ed8dd7d0231f56fe0e3970ca
Yaremenko Dmitry <deweisowl@gmail.com>
2013-03-29 20:51:26 +0200

    Added controllers tests

--- spec/controllers/alternative_responses_controller_spec.rb
+++ spec/controllers/alternative_responses_controller_spec.rb
@@ -1,1 +1,32 @@
 require 'spec_helper'
+
+describe AlternativeResponsesController do
+
+  before do
+    request.accept = 'application/json'
+	end
+
+  describe 'GET show' do
+    it 'shows alternative responses' do
+      scene = Scene.create! title: 'Title', description: 'Some scene'
+      reply_one = Response.create! response: 'Step 0.1.', scene_id: scene.id, parent_id: 0
+      reply_two = Response.create! response: 'Step 0.2.', scene_id: scene.id, parent_id: 0
+
+      get :show, scene_id: scene.id, id: reply_one.id
+
+      response.code.should eq '200'
+    end
+  end
+
+  describe 'POST create' do
+    it 'creates alternative response' do
+      scene = Scene.create! title: 'Title', description: 'Some scene'
+      reply = Response.create! response: 'Step 0.1.', scene_id: scene.id, parent_id: 0
+
+      post :create, scene_id: 1, id: reply.id, parent_id: 0, response: 'Some action of scenario'
+
+      response.code.should eq '200'
+    end
+  end
+
+end
