dfbabeaf86165ad37595ae3f80efdae536584ab6
Yaremenko Dmitry <deweisowl@gmail.com>
2013-04-20 09:04:18 +0300

    Added test for case when we post alternative response to first response and parent_id should equal to 0, but not nil

--- spec/controllers/alternative_responses_controller_spec.rb
+++ spec/controllers/alternative_responses_controller_spec.rb
@@ -23,9 +23,19 @@ describe AlternativeResponsesController do
       scene = Scene.create! title: 'Title', description: 'Some scene'
       reply = Response.create! response: 'Step 0.1.', scene_id: scene.id, parent_id: 0
 
-      post :create, scene_id: 1, id: reply.id, parent_id: 0, response: {response: 'Some action of scenario'}
+      post :create, scene_id: 1, id: reply.id, response: {response: 'Some action of scenario'}
 
       response.code.should eq '200'
+      scene.responses.last.parent_id.should eq 0
+    end
+
+    it 'creates alternative response with parent_id = 0, and not parent_id = nil' do
+      scene = Scene.create! title: 'Title', description: 'Some scene'
+      reply = Response.create! response: 'Step 0.1.', scene_id: scene.id, parent_id: 0
+
+      post :create, scene_id: 1, id: reply.id, response: {response: 'Some action of scenario'}
+
+      scene.responses.last.parent_id.should eq 0
     end
   end
 
